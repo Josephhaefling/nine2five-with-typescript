f5c3b8966683e63e0e2850a0a3e4de113f6940f5
Joe Haefling <josephhaefling@gmail.com>
2020-12-11 14:23:56 -0700

    Resolve merge conflict

--- Server/routes/posts.js
+++ Server/routes/posts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 
 import { getPosts } from "../controllers/posts.js"
@@ -9,16 +8,3 @@ const router = express.Router();
 router.get('/', getPosts)
 
 export default router;
-=======
-import express from "express"
-//Impor the controllers
-import { getPosts, createPost } from "../controllers/posts.js"
-
-const router = express.Router()
-//Initial get request
-router.get("/", getPosts)
-//Post request
-router.post("/", createPost)
-
-export default router
->>>>>>> dde11daf18f425cc56849f2303cbb99d17e8775b
